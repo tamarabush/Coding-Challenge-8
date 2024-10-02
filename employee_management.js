423414d76f366d6600f563e5a68e765010209d40
tamarabush <tamara.abush14@outlook.com>
2024-10-02 12:40:22 -0400

    Create Department class to manage employees and calculate salary

--- employee_management.js
+++ employee_management.js
@@ -10,8 +10,32 @@ class Employee {
     }
 
     getDetails() {
-        return (`${this.name} is a ${this.position}, who is earning $${this.salary} per year`);
+        return (`${this.name} is a ${this.position}, who is earning $${this.salary} per year.`);
     //using the get method to 'get' the data properties from the class employee
     }
 } 
 
+const employee1 = new Employee("Robb Stark", 175000, "Westeros King");
+console.log(employee1.getDetails()); 
+
+
+
+//TASK 2 - Create a Department Class:
+
+class department {
+    constructor(name) {
+        this.name = name
+        this.employees = []
+    //initialize name and employees in the class
+    }
+
+    addEmployees(employee) {
+        this.employees.push(employee);
+    //pushing the employee class in to the employees array 
+    }
+
+    get getDepartmentSalary() {
+        return this.employees.reduce((totalSalary, employee) => totalSalary + employee.salary, 0);
+    //using the reduce method to calculate the total salary 
+    }
+}
